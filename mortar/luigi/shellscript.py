48dba85bd289858ffa377d1163ed9fa81bb7d63c
Chris Ngan <chris.ngan24@gmail.com>
2014-03-26 13:39:55 -0400

    added shellscript tasks

---
+++ mortar/luigi/shellscript.py
@@ -0,0 +1,64 @@
+import luigi
+import os
+import abc 
+import sys
+import subprocess 
+import logging
+from luigi import LocalTarget
+from mortar.luigi import target_factory
+
+logger = logging.getLogger('luigi-interface')
+class ShellScriptTask(luigi.Task):
+    token_path = luigi.Parameter()
+
+    def output_token(self):
+        """
+        Token written out to indicate finished shell script
+        """
+        return target_factory.get_target('%s/%s' % (self.token_path, self.__class__.__name__))
+
+    def output(self):
+        return [self.output_token()]
+
+    @abc.abstractmethod
+    def subprocess_commands(self):
+        """
+        Shell commands that will be ran in a subprocess
+        Should return a string where each line of script is seperated with ';'
+        Example:
+            cd my/dir; ls;
+        """
+        raise RuntimeError("Must implement subprocess_commands!")
+    
+    def run(self):
+        if not self.output_token().exists():
+            cmd = self.subprocess_commands()
+            output = subprocess.Popen(
+                cmd,
+                shell=True,
+                stdout = subprocess.PIPE,
+                stderr = subprocess.PIPE
+            )
+            out, err = output.communicate()
+            # generate output message
+            message = ''
+            message += '\n-----------------------------'
+            message += '\nCMD    : %s' % cmd
+            message += '\nSTDOUT : %s' % repr(out)
+            message += '\nSTDERR : %s' % repr(err)
+            message += '\n-----------------------------'
+            self.check_error(err, message)
+
+            self.cmd_output = {
+              'cmd'   : cmd,
+              'stdout': out,
+              'stderr': err
+            }
+            logger.debug('%s - output:%s' % (self.__class__.__name__, message))
+            if err == '':
+                target_factory.write_file(self.output_token())
+
+    def check_error(self, err, message):
+        if err != '':
+            raise RuntimeError(message)
+            
---
+++ mortar/test/shellscript_test.py
@@ -0,0 +1,52 @@
+import unittest, luigi, tempfile, os 
+from luigi import LocalTarget
+from mortar.luigi.shellscript import ShellScriptTask
+
+class TestShellScriptTask(ShellScriptTask):
+    def subprocess_commands(self):
+        return 'echo hello-world; echo hello-world-again;'
+        
+class TestErrorShellScriptTask(ShellScriptTask):
+    def subprocess_commands(self):
+        return 'ls this-is-a-fake-file'
+
+class TestShellScript(unittest.TestCase):
+
+    def setUp(self):
+        self.token_path = tempfile.mkdtemp()
+
+
+    def tearDown(self):
+        path = self.t.output()[0].path
+        if os.path.isfile(path): 
+            os.remove(path)
+    
+    def test_output_token(self):
+        """
+        Output should be the given token path and the class name
+        """
+        t = TestShellScriptTask(self.token_path) 
+        self.t = t 
+        luigi.build([t], local_scheduler=True)
+        self.assertEquals('%s/%s' % (self.token_path, t.__class__.__name__), t.output()[0].path)
+
+    def test_run_process(self):
+        """
+        Check output is ran
+        """
+        t = TestShellScriptTask(self.token_path) 
+        self.t = t 
+        luigi.build([t], local_scheduler=True)
+        self.assertEquals(self.t.cmd_output['stdout'], 'hello-world\nhello-world-again\n')
+        self.assertEquals(self.t.cmd_output['cmd'], 'echo hello-world; echo hello-world-again;')
+        self.assertEquals(self.t.cmd_output['stderr'], '')
+
+
+    def test_run_error_throw(self):
+        """
+        Raise exception at error
+        """
+        t = TestErrorShellScriptTask(self.token_path) 
+        self.t = t 
+        self.assertRaises(RuntimeError, lambda: t.check_error('error', 'error message'))
+
