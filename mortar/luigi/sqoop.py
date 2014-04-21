75e7a07653a72e1f2769b716a34056bf98414cb5
Chris Ngan <chris.ngan24@gmail.com>
2014-04-21 13:09:33 -0400

    luigi tasks for all sqoop commands

--- mortar/luigi/sqoop.py
+++ mortar/luigi/sqoop.py
@@ -6,7 +6,20 @@ import logging
 logger = logging.getLogger('luigi-interface')
 
 class MortarSqoopTask(S3PathTask):
+    """
+    Base class for Mortar Sqoop commands
+    One required parameter:
+        path - s3n path where results are stored
+    Three optional parameters:
+        jdbc_driver - Name of the JDBC driver class (COM.DRIVER.BAR)
+        direct -   Use a direct import path
+        driver_jar - Path to the jar containing the jdbc driver
+    """
+
     # path is a parameter
+    jdbc_driver = luigi.Parameter(default=None)
+    direct = luigi.Parameter(default=None)
+    driver_jar = luigi.Parameter(default=None)
         
     def parameters(self):
         config = luigi.configuration.get_config()
@@ -17,6 +30,12 @@ class MortarSqoopTask(S3PathTask):
                 'port' : config.get('database', 'port', ''),
                 'username' : config.get('database', 'username'),
                 'password' : config.get('database', 'password')}
+
+    def aws_params(self):
+        config = luigi.configuration.get_config()
+        return {
+                'aws_access_key_id' : config.get('s3', 'aws_access_key_id'),
+                'aws_secret_access_key' : config.get('s3', 'aws_secret_access_key')}
 
     @abc.abstractmethod
     def command(self):
@@ -29,6 +48,16 @@ class MortarSqoopTask(S3PathTask):
     def options(self):
         return ''
 
+    def set_aws_keys(self):
+        aws_params = self.aws_params()
+        os.environ['AWS_ACCESS_KEY'] = aws_params['aws_access_key_id']
+        os.environ['AWS_SECRET_KEY'] = aws_params['aws_secret_access_key']
+
+    def _append_array_if_item_exists(self, array, item, option_key):
+        if item != None:
+            array.append('-%s %s' % (option_key, item)) 
+
+
     def run(self):
         params = self.parameters()
         command_str = [
@@ -40,26 +69,44 @@ class MortarSqoopTask(S3PathTask):
             self.path,
             '-u %s' % params['username'],
             '-p %s' % params['password'],
-            '--host %s' % (params['host'] + ':' + params['port']) if params['port'] != '' else params['host']]
+            '--host %s' % (params['host'] + ':' + params['port'] if params['port'] != '' else params['host'])]
+
+        self._append_array_if_item_exists(command_str, self.driver_jar, 'r')
+        self._append_array_if_item_exists(command_str, self.direct, 'd')
+        self._append_array_if_item_exists(command_str, self.jdbc_driver, 'j')
 
         command_str = ' '.join(command_str)
         self.command_str = command_str
+        self.set_aws_keys()
         logger.debug(command_str)
         os.system(command_str)
 
 
 class MortarSqoopQueryTask(MortarSqoopTask):
+    """
+    Export the result of an SQL query to S3.
+    Runs:
+    mortar local:sqoop_query dbtype database-name query s3-destination 
+
+    sql_query must be implemented with the query that is going to be run
+    """
+
     def command(self):
         return 'sqoop_query'
     
     def arguments(self):
-        return self.sql_query()
+        return '"%s"' % self.sql_query()
 
     @abc.abstractmethod
     def sql_query(self):
         raise RuntimeError("must implement sql query!")
 
 class MortarSqoopIncrementalTask(MortarSqoopTask):
+    """
+    Export all records where column is > value
+    Runs:
+    mortar local:sqoop_incremental dbtype database-name table column value s3-destination
+    """
     table = luigi.Parameter()
     column = luigi.Parameter()
     value = luigi.Parameter()
@@ -71,6 +118,11 @@ class MortarSqoopIncrementalTask(MortarSqoopTask):
         return '%s %s %s' % (self.table, self.column, self.value)
 
 class MortarSqoopTableTask(MortarSqoopTask):
+    """
+    Export data from an RDBMS table to S3.
+    Runs:
+    mortar local:sqoop_table dbtype database-name table s3-destination
+    """
     table = luigi.Parameter()
 
     def command(self):
--- mortar/test/sqoop_test.py
+++ mortar/test/sqoop_test.py
@@ -1,4 +1,5 @@
 import unittest
+import os
 from mock import patch
 from mortar.luigi.sqoop import MortarSqoopTask
 import luigi
@@ -11,6 +12,8 @@ PORT = '1234'
 USERNAME = 'myusername'
 PASSWORD = 'mypassword'
 S3_PATH = 's3n://my_bucket'
+AWS_ACCESS_KEY = 'key'
+AWS_SECRET_KEY = 'secret'
 
 class MortarSqoopTaskTest(MortarSqoopTask):
     def parameters(self):
@@ -20,6 +23,9 @@ class MortarSqoopTaskTest(MortarSqoopTask):
                 'port' : PORT, 
                 'username' : USERNAME, 
                 'password' : PASSWORD}
+    def aws_params(self):
+        return {'aws_access_key_id' : AWS_ACCESS_KEY,
+                'aws_secret_access_key' : AWS_SECRET_KEY}
 
     def command(self):
         return 'test command'
@@ -30,13 +36,54 @@ class MortarSqoopTaskTest(MortarSqoopTask):
         return 'extra arguments'
 
 
+
 EXPECTED_STR = 'mortar local:test command some_dbtype mydatabase extra arguments s3n://my_bucket -u myusername -p mypassword --host host:1234'
 
 class TestMortarSqoopBase(unittest.TestCase):
-    @mock_s3
     @patch("os.system")
     def test_run(self, os_mock):
         t = MortarSqoopTaskTest(path=S3_PATH)
         luigi.build([t], local_scheduler=True)
         self.assertEquals(t.command_str , EXPECTED_STR)
+        self.assertEquals(os.environ['AWS_ACCESS_KEY'], AWS_ACCESS_KEY)
+        self.assertEquals(os.environ['AWS_SECRET_KEY'], AWS_SECRET_KEY)
 
+    @patch("os.system")
+    def test_run_options_with_driver_jar(self, os_mock):
+        t = MortarSqoopTaskTest(path=S3_PATH, driver_jar='some/path')
+        luigi.build([t], local_scheduler=True)
+        option_string = EXPECTED_STR + ' -r some/path'
+        self.assertEqual(t.command_str , option_string)
+
+    @patch("os.system")
+    def test_run_options_with_jdbc_jar(self, os_mock):
+        t = MortarSqoopTaskTest(path=S3_PATH, jdbc_driver='some/path')
+        luigi.build([t], local_scheduler=True)
+        option_string = EXPECTED_STR + ' -j some/path'
+        self.assertEqual(t.command_str , option_string)
+
+    @patch("os.system")
+    def test_run_options_with_direct(self, os_mock):
+        t = MortarSqoopTaskTest(path=S3_PATH, direct='some/path')
+        luigi.build([t], local_scheduler=True)
+        option_string = EXPECTED_STR + ' -d some/path'
+        self.assertEqual(t.command_str , option_string)
+
+    @patch("os.system")
+    def test_run_options_with_all_direct(self, os_mock):
+        t = MortarSqoopTaskTest(path=S3_PATH, direct='some/path',
+                                jdbc_driver='jdbc/path', driver_jar='jar/path')
+        luigi.build([t], local_scheduler=True)
+        option_string = EXPECTED_STR + ' -r jar/path -d some/path -j jdbc/path'
+        self.assertEqual(t.command_str , option_string)
+
+
+
+
+
+
+
+
+
+    
+    
